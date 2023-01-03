2e2c3b4b14fec71a61f2fb52af348338cd483f2d
Alessia B <borys.alessia@gmail.com>
2023-01-03 18:01:06 +0100

    fullstack uppp!

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import filePath from "./filePath";
-import { Client } from 'pg';
+import { Client } from "pg";
 
 const app = express();
 app.use(express.json());
@@ -12,19 +12,19 @@ dotenv.config();
 interface PasteBinType {
   id: number;
   date: Date;
-  title: null | string
+  title: null | string;
   body: string;
 }
 
 interface PostType {
   date: Date;
-  title: null | string
+  title: null | string;
   body: string;
 }
 
 const PORT_NUMBER = process.env.PORT ?? 4000;
-const client = new Client(process.env.DATABASE_URL)
-client.connect()
+const client = new Client(process.env.DATABASE_URL);
+client.connect();
 
 app.get("/", (req, res) => {
   const pathToFile = filePath("../public/index.html");
@@ -34,56 +34,62 @@ app.get("/", (req, res) => {
 app.get("/pastes", async (req, res) => {
   try {
     const queryResponse = await client.query(`
-SELECT *
-FROM paste_bin_data
-ORDER BY date desc
-LIMIT 10
-`)
-    const allPastes = queryResponse.rows
-    res.status(200).json(allPastes)
-  }
-  catch (error) {
-    console.error(error)
-    res.status(404).json({message: "internal error"})
+    SELECT *
+    FROM paste_bin_data
+    ORDER BY date DESC
+    LIMIT 10
+    `);
+    const allPastes = queryResponse.rows;
+    res.status(200).json(allPastes);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ message: "internal error" });
   }
 });
 
 app.post<{}, {}, PasteBinType>("/pastes", async (req, res) => {
   try {
-    const values = [req.body.body, req.body.title]
-    const queryResponse = await client.query(`
+    let values = [req.body.body, req.body.title];
+    if (!req.body.title) {
+      values = [req.body.body, null];
+    }
+    const queryResponse = await client.query(
+      `
     INSERT INTO paste_bin_data (body, title)
     VALUES (
       $1, $2
     )
     RETURNING *
-    `, values);
-    const createdPaste = queryResponse.rows[0]
-    res.status(200).json(createdPaste)
+    `,
+      values
+    );
+    const createdPaste = queryResponse.rows[0];
+    res.status(200).json(createdPaste);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ message: "internal error" });
   }
-  catch(error) {
-    console.error(error)
-    res.status(404).json({message: "internal error"})
-  }
-})
+});
 
 app.get<{ id: string }>("/pastes/:id", async (req, res) => {
   try {
-    const id = req.params.id
-    const values = [id]
-    const queryResponse = await client.query(`
+    const id = req.params.id;
+    const values = [id];
+    const queryResponse = await client.query(
+      `
     SELECT *
     FROM paste_bin_data
     WHERE id = $1
-    `, values);
-    const singlePaste = queryResponse.rows[0]
-    res.status(200).json(singlePaste)
+    `,
+      values
+    );
+    const singlePaste = queryResponse.rows[0];
+    res.status(200).json(singlePaste);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ message: "internal error" });
   }
-  catch(error) {
-    console.error(error)
-    res.status(404).json({message: "internal error"})
-  }
-})
+});
 
 app.listen(PORT_NUMBER, () => {
   console.log(`Server is listening on port ${PORT_NUMBER}!`);
